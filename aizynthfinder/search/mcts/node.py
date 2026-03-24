9702bc173293ef016e04e31998dacc453a694d66
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-24 09:28:10 +0000

    Revert "Deduplicate sibling cache molecules in MCTS"
    This reverts commit 9ad09b3f8b353d4d496e0356bffe5a9dd268dadb.

--- aizynthfinder/search/mcts/node.py
+++ aizynthfinder/search/mcts/node.py
@@ -246,14 +246,9 @@ class MctsNode:
 
         cache_molecules = []
         if self.parent:
-            seen_cache_keys = set()
             for child in self.parent.children:
                 if child is not self:
-                    for mol in child.state.expandable_mols:
-                        if mol.inchi_key in seen_cache_keys:
-                            continue
-                        seen_cache_keys.add(mol.inchi_key)
-                        cache_molecules.append(mol)
+                    cache_molecules.extend(child.state.expandable_mols)
 
         # Calculate the possible actions, fill the child_info lists
         # Actions by default only assumes 1 set of reactants
