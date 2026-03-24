f278a85f91b65c9c780c9a4222c365bf8ece4a22
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-24 13:21:23 +0000

    Revert "Allow one extra transform for near-solved states"
    This reverts commit d6199e506e5d1d1b402958d084046260ea8e7fd8.

--- aizynthfinder/search/mcts/state.py
+++ aizynthfinder/search/mcts/state.py
@@ -50,11 +50,8 @@ class MctsState:
         self._stock_availability: Optional[List[str]] = None
         self.is_solved = all(self.in_stock_list)
         self.max_transforms = max(mol.transform for mol in self.mols)
-        max_transforms_limit = config.search.max_transforms
-        if len(self.expandable_mols) <= 1:
-            max_transforms_limit += 1
         self.is_terminal = (
-            self.max_transforms >= max_transforms_limit
+            self.max_transforms >= config.search.max_transforms
         ) or self.is_solved
 
         inchis = [mol.inchi_key for mol in self.mols]
