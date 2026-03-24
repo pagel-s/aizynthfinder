4e56e79829ff3e4b1383a82d687662a7cc7848d9
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-24 08:51:05 +0000

    Enable immediate MCTS child instantiation

--- aizynthfinder/context/config.py
+++ aizynthfinder/context/config.py
@@ -38,7 +38,7 @@ class _SearchConfiguration:
             "use_prior": True,
             "prune_cycles_in_search": True,
             "search_rewards": ["state score"],
-            "immediate_instantiation": (),
+            "immediate_instantiation": ("uspto",),
             "mcts_grouping": "partial",
             "search_rewards_weights": [],
         }
