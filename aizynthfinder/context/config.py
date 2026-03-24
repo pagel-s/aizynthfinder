2f899fedfad313d77b0f2598ab112eb565da54c1
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-24 08:50:13 +0000

    Revert "Disable MCTS prior initialization"
    This reverts commit 289d3ccbd8a83fbd7641272f6620595f0a76d605.

--- aizynthfinder/context/config.py
+++ aizynthfinder/context/config.py
@@ -35,7 +35,7 @@ class _SearchConfiguration:
         default_factory=lambda: {
             "C": 0.4,
             "default_prior": 0.5,
-            "use_prior": False,
+            "use_prior": True,
             "prune_cycles_in_search": True,
             "search_rewards": ["state score"],
             "immediate_instantiation": (),
