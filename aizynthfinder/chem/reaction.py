7a158eb4c448803a69ef256063c02726b555be79
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-24 13:49:13 +0000

    Revert "Cache computed reaction SMILES"
    This reverts commit 51aa2a94ec248e420bef196e7d817841421ddca1.

--- aizynthfinder/chem/reaction.py
+++ aizynthfinder/chem/reaction.py
@@ -111,11 +111,9 @@ class _ReactionInterfaceMixin:
 
         :return: the SMILES
         """
-        if getattr(self, "_reaction_smiles", None) is None:
-            reactants = ".".join(mol.smiles for mol in self._reactants_getter())  # type: ignore
-            products = ".".join(mol.smiles for mol in self._products_getter())  # type: ignore
-            self._reaction_smiles = f"{reactants}>>{products}"
-        return self._reaction_smiles
+        reactants = ".".join(mol.smiles for mol in self._reactants_getter())  # type: ignore
+        products = ".".join(mol.smiles for mol in self._products_getter())  # type: ignore
+        return f"{reactants}>>{products}"
 
 
 class RetroReaction(abc.ABC, _ReactionInterfaceMixin):
@@ -155,7 +153,6 @@ class RetroReaction(abc.ABC, _ReactionInterfaceMixin):
         self.metadata: StrDict = metadata or {}
         self._reactants: Optional[Tuple[Tuple[TreeMolecule, ...], ...]] = None
         self._smiles: Optional[str] = None
-        self._reaction_smiles: Optional[str] = None
         self._kwargs: StrDict = kwargs
 
     @classmethod
@@ -225,7 +222,6 @@ class RetroReaction(abc.ABC, _ReactionInterfaceMixin):
         )
         new_reaction._reactants = tuple(mol_list for mol_list in self._reactants or [])
         new_reaction._smiles = self._smiles
-        new_reaction._reaction_smiles = self._reaction_smiles
         return new_reaction
 
     def mapped_reaction_smiles(self) -> str:
