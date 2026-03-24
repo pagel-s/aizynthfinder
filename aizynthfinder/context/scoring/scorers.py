7e806578324ca9317073988e2e76900eef9c0d7e
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-24 15:18:14 +0000

    Revert "reward unresolved precursor count"
    This reverts commit a1b8c91844bb12e1b37e67a89b8a4bfdf5ed5b93.

--- aizynthfinder/context/scoring/scorers.py
+++ aizynthfinder/context/scoring/scorers.py
@@ -27,6 +27,7 @@ from aizynthfinder.context.scoring.scorers_mols import (
     FractionInStockScorer,
     FractionOfIntermediatesInStockScorer,
     NumberOfPrecursorsInStockScorer,
+    NumberOfPrecursorsScorer,
     PriceSumScorer,
     StockAvailabilityScorer,
 )
@@ -127,32 +128,27 @@ class StateScorer(Scorer):
             scaler_params={"name": "squash", "slope": -1, "yoffset": 0, "xoffset": 4},
         )
         self._in_stock_scorer = FractionInStockScorer(config)
+        self._precursor_scorer = NumberOfPrecursorsScorer(
+            config,
+            scaler_params={"name": "squash", "slope": -1, "yoffset": 0, "xoffset": 4},
+        )
 
     def _score(self, item: _Scoreable) -> float:
         in_stock_fraction = self._in_stock_scorer(item)
         max_transform = self._transform_scorer(item)
-        unresolved_precursor_score = self._unresolved_precursor_score(item)
+        precursor_score = self._precursor_scorer(item)
         # A scorer can return a list of float if the item is a list of trees/nodes,
         # but that is not the case here. However this is needed because of mypy
         assert (
             isinstance(in_stock_fraction, float)
             and isinstance(max_transform, float)
-            and isinstance(unresolved_precursor_score, float)
+            and isinstance(precursor_score, float)
         )
         return (
             0.85 * in_stock_fraction
             + 0.05 * max_transform
-            + 0.10 * unresolved_precursor_score
+            + 0.10 * precursor_score
         )
-
-    def _unresolved_precursor_score(self, item: _Scoreable) -> float:
-        if isinstance(item, MctsNode):
-            unresolved = len(item.state.expandable_mols)
-        else:
-            unresolved = len(
-                [leaf for leaf in item.leafs() if leaf not in self._config.stock]
-            )
-        return float(1 / (1 + np.exp(unresolved - 2)))
 
     def _score_node(self, node: MctsNode) -> float:
         return self._score(node)
