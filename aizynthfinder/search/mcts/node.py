3ad923ee6516ed4372572a960947ebb2899eb334
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-25 14:09:42 +0000

    Restore kept baseline after discarded node unfreeze

--- aizynthfinder/search/mcts/node.py
+++ aizynthfinder/search/mcts/node.py
@@ -68,7 +68,7 @@ class MctsNode:
         self._filter_policy = config.filter_policy
         self.tree = owner
         self.is_expanded: bool = False
-        self.is_expandable: bool = not self.state.is_solved
+        self.is_expandable: bool = not self.state.is_terminal
         self._parent = parent
 
         if owner is None:
@@ -256,7 +256,7 @@ class MctsNode:
             self._logger.debug(msg)
             raise NodeUnexpectedBehaviourException(msg)
 
-        if self.is_expanded or self.is_terminal():
+        if self.is_expanded or not self.is_expandable:
             return
 
         self.is_expanded = True
