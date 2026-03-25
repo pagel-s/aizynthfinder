1af438ae01044f4cd0fca73986bf3521f34d7ebb
Sebastian Pagel <pagel.sebastian.1@gmail.com>
2026-03-25 09:27:00 +0000

    Late-activate tree-wide duplicate MCTS pruning

--- aizynthfinder/search/mcts/node.py
+++ aizynthfinder/search/mcts/node.py
@@ -402,6 +402,11 @@ class MctsNode:
                     self._disable_child(child_idx)
                 continue
 
+            if self._generated_tree_degeneracy(state):
+                if state_index == 0:
+                    self._disable_child(child_idx)
+                continue
+
             # If there's more than one outcome, the lists need be expanded
             if state_index > 0:
                 child_idx = self._expand_children_lists(first_child_idx, state_index)
@@ -502,6 +507,16 @@ class MctsNode:
             previous_action.metadata["additional_actions"] = []
         previous_action.metadata["additional_actions"].append(metadata_copy)
         return True
+
+    def _generated_tree_degeneracy(self, new_state: MctsState) -> bool:
+        if self._degeneracy_check not in ["partial", "full"] or not self.tree:
+            return False
+        tree_duplicate_pruning_start_iteration = self._algo_config.get(
+            "tree_duplicate_pruning_start_iteration", 250
+        )
+        if self.tree.profiling["iterations"] < tree_duplicate_pruning_start_iteration:
+            return False
+        return not self.tree.register_state(new_state, self._degeneracy_check)
 
     def _instantiate_child(self, child_idx: int) -> List["MctsNode"]:
         """
